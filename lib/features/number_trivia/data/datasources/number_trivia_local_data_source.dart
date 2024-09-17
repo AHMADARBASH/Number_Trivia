e0e9c7964e12bd52265fbbddb080c73bb65f8ebb
Ahmad Arbash <ahmad.arbash.97@outlook.com>
2024-09-17 19:49:54 +0300

    local data source implementation

--- lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,8 +1,33 @@
+import 'dart:convert';
+
+import '../../../../core/error/excpetions.dart';
 import '../../domain/entities/number_trivia.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
   Future<void> cacheNumberTrivia(NumberTrivia numberTrivia);
+}
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences cache;
+  NumberTriviaLocalDataSourceImpl({required this.cache});
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTrivia numberTrivia) async {
+    await cache.setString('cached_number_trivia', json.encode(numberTrivia));
+  }
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final jsonString = cache.getString('cached_number_trivia');
+    if (jsonString != null) {
+      return Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)));
+    } else {
+      throw CacheException();
+    }
+  }
 }
